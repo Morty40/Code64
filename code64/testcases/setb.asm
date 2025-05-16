b80ff73c246e17dd9dadcb9bfc7a2ff7d366dcab
Morty40 <morten@perriard.dk>
2025-05-16 21:13:41 +0200

    removing setb/setw

--- code64/testcases/setb.asm
+++
@@ -1,7 +0,0 @@
-            .org $1000
-            @setb(dest1, 1)
-            @setb(dest2, [value])
-            rts
-value:      .byte 2
-dest1:      .byte 0
-dest2:      .byte 0
--- code64/testcases/setw.asm
+++
@@ -1,7 +0,0 @@
-            .org $1000
-            @setw(dest1, 1)
-            @setw(dest2, [value])
-            rts
-value:      .word 2
-dest1:      .word 0
-dest2:      .word 0
